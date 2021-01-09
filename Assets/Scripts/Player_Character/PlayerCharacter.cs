3982442765b70185171260a2c4daedd357d6c2b5
Jeksome <jeksome@gmail.com>
2021-01-09 13:26:37 +0200

    0.2.0
    Garbage files deleted
    Interior and exterior texture assets added
    World light and shadows adjusted
    Hit detection animation commented

---
+++ Assets/Scripts/Player_Character/PlayerCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health;
+    private bool isAlive;
+    void Start()
+    {
+        _health = 5;
+        isAlive = true;
+    }
+
+    void Update()
+    {
+        if (_health < 1 && isAlive)
+        {
+            Death();
+            isAlive = false;
+        }
+    }
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Health: " + _health);
+    }
+
+    public void Death()
+    {
+        Debug.Log("You are dead");
+    }
+}
